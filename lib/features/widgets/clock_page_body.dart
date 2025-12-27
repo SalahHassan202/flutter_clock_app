18c2b5af8e6d722ac34f1bb93dc49ed0ba87f89b
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-28 01:00:13 +0200

    finish clock page body file

---
+++ lib/features/widgets/clock_page_body.dart
@@ -0,0 +1,139 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_app/core/colors/app_colors.dart';
+import 'package:one_clock/one_clock.dart';
+import 'package:segment_display/segment_display.dart';
+
+class ClockPageBody extends StatefulWidget {
+  final String clockType;
+  final Color clockColor;
+  final double clockSize;
+
+  const ClockPageBody({
+    super.key,
+    required this.clockType,
+    required this.clockColor,
+    required this.clockSize,
+  });
+
+  @override
+  State<ClockPageBody> createState() => _ClockPageBodyState();
+}
+
+class _ClockPageBodyState extends State<ClockPageBody> {
+  late Timer _timer;
+  late DateTime _now;
+
+  @override
+  void initState() {
+    super.initState();
+    _now = DateTime.now();
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      setState(() {
+        _now = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    switch (widget.clockType) {
+      case 'Analog':
+        return _buildAnalogClock();
+      case 'Digital':
+        return _buildDigitalClock();
+      case 'Text':
+      default:
+        return _buildTextClock(context);
+    }
+  }
+
+  Widget _buildAnalogClock() {
+    return Center(
+      child: AnalogClock(
+        isLive: true,
+        width: widget.clockSize,
+        hourHandColor: widget.clockColor,
+        minuteHandColor: widget.clockColor,
+        secondHandColor: widget.clockColor,
+        numberColor: widget.clockColor,
+        showNumbers: true,
+        showAllNumbers: true,
+        textScaleFactor: 1.5,
+        showTicks: true,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: AppColors.whiteColor,
+          border: Border.all(color: widget.clockColor, width: 2),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDigitalClock() {
+    final timeString =
+        '${_now.hour.toString().padLeft(2, '0')}:'
+        '${_now.minute.toString().padLeft(2, '0')}:'
+        '${_now.second.toString().padLeft(2, '0')}';
+
+    return Center(
+      child: SevenSegmentDisplay(
+        value: timeString,
+        size: widget.clockSize / 50,
+        characterSpacing: 6.0,
+        backgroundColor: Colors.transparent,
+        segmentStyle: HexSegmentStyle(
+          enabledColor: widget.clockColor,
+          // ignore: deprecated_member_use
+          disabledColor: widget.clockColor.withOpacity(0.15),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextClock(BuildContext context) {
+    final timeString = TimeOfDay.now().format(context);
+
+    return Center(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          // ignore: deprecated_member_use
+          color: Colors.grey.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              // ignore: deprecated_member_use
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 8,
+              offset: const Offset(2, 4),
+            ),
+          ],
+        ),
+        child: Text(
+          'It is $timeString',
+          style: TextStyle(
+            fontSize: widget.clockSize / 8,
+            color: widget.clockColor,
+            fontWeight: FontWeight.bold,
+            shadows: [
+              Shadow(
+                offset: const Offset(1, 2),
+                blurRadius: 2.0,
+                // ignore: deprecated_member_use
+                color: Colors.black.withOpacity(0.2),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
