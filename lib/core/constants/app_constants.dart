452cdbe8fd0a285f6d0e132a3993067c27a8c1b0
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-27 18:17:23 +0200

    add app constant file

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_app/core/colors/app_colors.dart';
+
+class AppConstants {
+  static const List<Color> colors = [
+    AppColors.greyColor,
+    AppColors.redColor,
+    AppColors.orangeColor,
+    AppColors.greenColor,
+    AppColors.purpleColor,
+    AppColors.blueColor,
+    AppColors.yellowColor,
+  ];
+}
