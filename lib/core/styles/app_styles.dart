93f186dd2efb36d20345a9f9ef6f5700b01c31d5
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-27 18:21:46 +0200

    add app styles file

---
+++ lib/core/styles/app_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_app/core/colors/app_colors.dart';
+
+class AppStyles {
+  static final TextStyle textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+
+  static final TextStyle textStyleBold = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    color: AppColors.blackColor,
+  );
+}
