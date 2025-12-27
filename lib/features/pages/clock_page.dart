4bc0e4594cc70659b781131f2e595040784f54fd
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-28 00:02:14 +0200

    added clock page

---
+++ lib/features/pages/clock_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClockPage extends StatelessWidget {
+  const ClockPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
