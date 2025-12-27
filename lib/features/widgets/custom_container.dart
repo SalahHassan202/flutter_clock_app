6ebb06ebfa48d4123fee7011d836e70c6c9ce7ea
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-28 00:22:18 +0200

    added custom container widget

--- lib/features/widgets/custom_container.dart
+++ lib/features/widgets/custom_container.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_app/core/colors/app_colors.dart';
+import 'package:flutter_clock_app/core/styles/app_styles.dart';
 
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 130,
+      padding: EdgeInsets.only(left: 30),
+      color: AppColors.blueColor,
+      child: Row(
+        children: [
+          const Icon(
+            Icons.access_time_filled_outlined,
+            color: AppColors.whiteColor,
+          ),
+          SizedBox(width: 20),
+          Text(
+            'Settings',
+            style: AppStyles.textStyleBold.copyWith(
+              color: AppColors.whiteColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
